ceb37786266a02611cda5aaf736ee9518e16f4da
Tulio Cruz <tulioocruz@gmail.com>
2022-07-04 19:14:13 -0300

    Add files via upload

---
+++ ClasseA.java
@@ -0,0 +1,29 @@
+public class ClasseA {
+
+    ClasseA classeA;
+    private int a1;
+    private float a2;
+
+    public int getA1() {
+        return a1;
+    }
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+    public float getA2() {
+        return a2;
+    }
+    public void setA2(float a2) {
+        this.a2 = a2;
+    }
+
+    public void mA1(){
+        System.out.println("mA1");
+    }
+
+    public void mA2(){
+        System.out.println("mA2");
+    }
+
+
+}
---
+++ ClasseB.java
@@ -0,0 +1,26 @@
+public class ClasseB {
+    
+    private int b1;
+    private float b2;
+
+    public int getB1() {
+        return b1;
+    }
+    public void setB1(int b1) {
+        this.b1 = b1;
+    }
+    public float getB2() {
+        return b2;
+    }
+    public void setB2(float b2) {
+        this.b2 = b2;
+    }
+
+    public void mB1(){
+        System.out.println("mB1");
+    }
+
+    public void mB2(){
+        System.out.println("mB2");
+    }
+}
