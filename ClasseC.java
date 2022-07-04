e6a807983384fe0ec46fd33b8a95157e2c2f14d2
Tulio <tuliocruz_@hotmail.com>
2022-07-04 20:02:29 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,12 @@
+public class ClasseC {
+    private String c1;
+    private int c2;
+
+    public void mc1(){
+        System.out.println("mc1");
+    }
+
+    public void mc2(){
+        System.out.println("mc2");
+    }
+}
