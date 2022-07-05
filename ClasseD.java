8906ca0b68dcaee9a6816818d84131c810859edd
Tulio <tuliocruz_@hotmail.com>
2022-07-05 08:51:32 -0300

    Criação da Classe D

---
+++ ClasseD.java
@@ -0,0 +1,13 @@
+public class ClasseD {
+    private int d1 = 10;
+    private int d2 = 12;
+
+    public int mD1(){
+        return d1;
+    }
+
+    public int mD2(){
+        return d2;
+    }
+
+}
